78a7557092154108f2f35b2443b388578bc461c6
Yousefatt <yousefyaser852@gmail.com>
2024-10-28 23:54:10 +0300

    edit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,9 @@
-'''
-Write a function named index_range that takes two integer arguments page and page_size.
 
-The function should return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.
+#Write a function named index_range that takes two integer arguments page and page_size.
 
-Page numbers are 1-indexed, i.e. the first page is page 1.
-'''
+#The function should return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters.
+
+#Page numbers are 1-indexed, i.e. the first page is page 1.
 
 def index_range(page, page_size):
     start = (page - 1) * page_size
@@ -15,7 +14,7 @@ def index_range(page, page_size):
 
     return range_tuple
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     res = index_range(1, 7)
     print(type(res))
     print(res)
